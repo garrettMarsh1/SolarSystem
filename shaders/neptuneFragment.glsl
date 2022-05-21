0ee007f7000545919fd4877ea9f35a4a21d4bc39
Garrett Marsh <garrett.marsh95@gmail.com>
2022-05-20 21:37:08 -0400

    commit

--- shaders/neptuneFragment.glsl
+++ shaders/neptuneFragment.glsl
@@ -4,7 +4,7 @@ varying vec3 vertexNormal;
 
 void main() {
   float intensity = 1.05 - dot(vertexNormal,
-vec3(0.7569, 0.7569, 0.8588));
+vec3(0.2353, 0.2353, 0.6588));
      vec3 neptune = vec3(0.5412, 0.6275, 0.7412)
       * pow(intensity, 1.5);
 
